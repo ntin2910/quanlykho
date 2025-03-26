1f45bcc419f896570ccad39fcc632e0b98e5db77
ntin2910 <ntin93960@gmail.com>
2025-03-26 09:07:05 +0700

    quanlykho

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <nav v-if="!isLoginPage">
+      <Router-Link to ="/pos01">Quản lý sản phẩm</Router-Link>
+      <Router-Link to ="/pos02">Nhập hàng</Router-Link>
+      <Router-Link to ="/pos03">Lịch sử nhập</Router-Link>
+      <Router-Link to ="/pos04">Tạo hóa đơn</Router-Link>
+      <Router-Link to ="/pos05">Hóa đơn</Router-Link>
+    </nav>
+  <router-view/>
+  </div>
+</template>
+
+<script>
+
+  export default{
+    name: 'App',
+    computed:{
+      isLoginPage(){
+        return this.$route && this.$route.path === '/login';
+      }
+    }
+  }
+</script>
